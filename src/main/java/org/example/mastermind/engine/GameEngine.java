6b5c187413230d584c8f9ed12f4bd4802cfa9d0c
nikholt1 <niklas1997@live.dk>
2026-02-26 11:41:03 +0100

    game engine setup

--- src/main/java/org/example/mastermind/engine/GameEngine.java
+++ src/main/java/org/example/mastermind/engine/GameEngine.java
@@ -3,8 +3,61 @@ package org.example.mastermind.engine;
 
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class GameEngine {
 
+    int amountOfGuesses;
+    int currentGuesses = 0;
+    public GameEngine() {}
 
+
+
+    public List<Integer> combination;
+
+
+    public void setGuesses(int amountOfGuesses) {
+        this.amountOfGuesses = amountOfGuesses;
+    }
+    public int getAmountOfGuesses() {
+        return amountOfGuesses;
+    }
+
+    public void setCombination(List<Integer> combination) {
+        this.combination = combination;
+    }
+    public List<Integer> getCombination() {
+        return combination;
+    }
+
+    public List<Integer> makeGuess(List<Integer> guess) {
+        // guess = 12345
+        // combination = 54321
+        // returner = 11211
+
+        for (int i = guess.size(); ) {
+
+        }
+
+
+    }
+
+
+
+//    public List<Integer> getCombination() {
+//        return gameEngine.getCombination(); // random combi fra computer
+//    }
+//
+//    public List<Integer> makeGuess(List<Integer> guess) {
+//        return gameEngine.makeGuess(guess); // sammenligner guess me combination -- sender 1 0 0 1 0? tilbage?
+//    } // 1 1 1 1 1 vundet --> 00000 ingen rigtige.
+//
+//    public List<Integer> makeCombination(List<Integer> userCombination) {
+//        return gameEngine.makeCombination(); // sætter combination til det brugeren har defineret
+//    }
+//
+//    public boolean resetGame() {
+//        return gameEngine.resetGame(); // sætter combination til .empty
+//    }
 }
--- src/main/java/org/example/mastermind/service/GameService.java
+++ src/main/java/org/example/mastermind/service/GameService.java
@@ -16,7 +16,9 @@ public class GameService {
         this.gameEngine = gameEngine;
     }
 
-
+    public void gameSetup(int amountOfGuesses) {
+        gameEngine.setGuesses(amountOfGuesses);
+    } // {1, 2, 3, 4, 5, 6}
 
     public List<Integer> getCombination() {
         return gameEngine.getCombination(); // random combi fra computer
@@ -24,7 +26,7 @@ public class GameService {
 
     public List<Integer> makeGuess(List<Integer> guess) {
         return gameEngine.makeGuess(guess); // sammenligner guess me combination -- sender 1 0 0 1 0? tilbage?
-    } // 1 1 1 1 1 vundet --> 00000 ingen rigtige.
+    } // 1 1 1 1 1 vundet --> 00000 ingen rigtige. // 1 0 1 2  1 = correct colour but wrong place 2 = right colour right place 0 = not correct
 
     public List<Integer> makeCombination(List<Integer> userCombination) {
         return gameEngine.makeCombination(); // sætter combination til det brugeren har defineret
