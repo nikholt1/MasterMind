caf61ea70af368f9c5cfca8f70cec49d2a94c1de
nikholt1 <niklas1997@live.dk>
2026-02-26 11:11:03 +0100

    init commit and push

---
+++ src/test/java/org/example/mastermind/MasterMindApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mastermind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterMindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
